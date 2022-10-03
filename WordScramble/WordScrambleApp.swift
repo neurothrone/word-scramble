fc695e323bb93807f2aa050d3ebf1db15e8badc6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-03 12:07:31 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Zaid Neurothrone on 2022-10-03.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
